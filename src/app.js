94717feb7f17cd39db5e4680640954ab77e1ad8e
SojitraMit <sojitramit57757@gmail.com>
2026-01-11 11:19:10 +0530

    validation

--- src/app.js
+++ src/app.js
@@ -3,19 +3,81 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "mit",
-    lastName: "sojtra",
-    emailId: "mit@gmail.com",
-    password: "mit@12345",
-  });
+  const user = new User(req.body);
+  if (req.body?.skills?.length > 10) {
+    throw new Error("Skils cannot be more than 10 ");
+  }
 
   try {
     await user.save();
     res.send("User adding successfully !!");
   } catch (err) {
-    res.send("Error saving the user" + err.message);
+    res.send("Error saving the user " + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+
+    res.send("user deleted uccessfully");
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpadateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpadateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if (data?.skills?.length > 10) {
+      throw new Error("Skils cannot be more than 10 ");
+    }
+
+    await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "before",
+      runValidators: true,
+    });
+    res.send("user updated successfully !! ");
+  } catch (err) {
+    res.status(400).send("Update failed :" + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,69 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userShema = mongoose.Schema({
-  firstName: {
-    type: String,
+const userShema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password ");
+        }
+      },
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("gender data is not valid");
+        }
+      },
+      min: 16,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://icon-library.com/images/laptop-user-icon/laptop-user-icon-16.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("photoUrl  is not valid ");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "Hi, I am using DevTinder.",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userShema);
